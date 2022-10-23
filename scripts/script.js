a2fe00fe7f833f17ca878c3d8ef839e28f59b815
Kseniya <ksuta_marfa@mail.ru>
2022-10-23 17:55:50 +0300

    11

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 let popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
-const popupOpenButtonElement = document.querySelector('.menu__open-popup');
+const popupOpenButtonElement = document.querySelector('.profile__open-popup');
 const popupSubmitButton = popupElement.querySelector('.popup__submit-button');
-let formElement = document.querySelector('.popup__conteiner');
+let formElement = document.querySelector('.popup__container');
 let nameInput = popupElement.querySelector('.popup__name');
 let infoInput = popupElement.querySelector('.popup__info');
 const profileElement = document.querySelector('.profile');
@@ -11,29 +11,36 @@ let infoProfile = profileElement.querySelector('.profile__subtitle');
 console.log(popupOpenButtonElement);
 
 const openPopup = function(event) {
-    console.log(event.target, event.currentTarget);
-    popupElement.classList.add('popup_is-opened');
+    // console.log(event.target, event.currentTarget);
+    popupElement.classList.add('popup__is-opened');
 }
 
 const closePopupByClickOnOverlay = function(event) {
-    console.log(event.target, event.currentTarget);
+    // console.log(event.target, event.currentTarget);
     if(event.target !== event.currentTarget) {
         return;
     }
     closePopup();
 }
 
-function addName(evt) {
-    evt.preventDefault();
+function addName(event) {
+    event.preventDefault();
     if (nameInput.value !== '') {
         nameProfile.textContent = nameInput.value;
-    } else {
-        closePopup();
-    }
+    } 
+    closePopup();
+}
+
+function addInfo(event) {
+    event.preventDefault();
+    if (infoInput.value !== '') {
+        infoProfile.textContent = infoInput.value;
+    } 
+    closePopup();
 }
 
 const closePopup = function() {
-    popupElement.classList.remove('popup_is-opened');
+    popupElement.classList.remove('popup__is-opened');
 }
 
 popupOpenButtonElement.addEventListener('click', openPopup);
@@ -42,4 +49,7 @@ popupElement.addEventListener('click', closePopupByClickOnOverlay);
 profileElement.addEventListener('click', function() {
     console.log('Profile clicked');
 })
-
+popupSubmitButton.addEventListener('click', addName);
+formElement.addEventListener('submit', addName);
+popupSubmitButton.addEventListener('click', addInfo);
+formElement.addEventListener('submit', addInfo);
