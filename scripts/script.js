ca7a2f619c450f384425e503279b77593d406e64
Kseniya <ksuta_marfa@mail.ru>
2022-11-02 21:03:16 +0300

    Added header

--- scripts/script.js
+++ scripts/script.js
@@ -8,10 +8,25 @@ let infoInput = popupElement.querySelector('.popup__info');
 const profileElement = document.querySelector('.profile');
 let nameProfile = profileElement.querySelector('.profile__title');
 let infoProfile = profileElement.querySelector('.profile__subtitle');
+let unlock = true;
+
+let popupEl = document.querySelector('.popup-plus');
+const popupCloseButtonEl = popupEl.querySelector('.popup-plus__close-button');
+const popupOpenButtonEl = document.querySelector('.profile__button');
+const popupSubmitButtonEl = popupEl.querySelector('.popup-plus__submit-button');
+let formElementEl = document.querySelector('.popup-plus__container');
+let nameInputEL = popupEl.querySelector('.popup-plus__name');
+let infoInputEl = popupEl.querySelector('.popup-plus__info');
+let nameProfileEl = document.querySelector('.element__title');
+let infoProfileEl = document.querySelector('.element__image');
+const card = document.querySelector('.element');
 
 const openPopup = function(event) {
-    popupElement.classList.add('popup__is-opened');
-    
+    popupElement.classList.add('popup__is-opened');      
+}
+
+const openPopupEl = function(event) {
+    popupEl.classList.add('popup__is-opened');      
 }
 
 function addProfile(event) {
@@ -24,12 +39,86 @@ function addProfile(event) {
     }
 }
 
+function addSpot(event) {
+  event.preventDefault();
+  let spot = {
+      'name': '',
+      'link': ''
+  };
+  if (nameInputEl.value !== '') {
+      spot.name = nameInputEl.value;
+  };
+  if (infoInput.value !== '') {
+      spot.link = infoInputEl.value;
+  };
+  renderImg(spot);
+  closePopupEl();
+}
+
 const closePopup = function() {
     popupElement.classList.remove('popup__is-opened');
     nameInput.value = nameProfile.textContent;
     infoInput.value = infoProfile.textContent;
 }
 
+
+const closePopupEl = function() {
+    popupEl.classList.remove('popup__is-opened');
+    nameInputEl.value = '';
+    infoInputEl.value = '';
+}
+
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 popupSubmitButton.addEventListener('click', addProfile);
+popupOpenButtonEl.addEventListener('click', openPopupEl);
+popupCloseButtonEl.addEventListener('click', closePopupEl);
+popupSubmitButtonEl.addEventListener('click', addSpot);
+
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+  function cloneElement(spot) {
+    const cardTemplate = document.querySelector('.spot');
+    const el = cardTemplate.content.cloneNode(true);
+    const textEl = el.querySelector('.element__title');
+    const imgEl = el.querySelector('.element__image');
+    textEl.textContent = spot.name;
+    imgEl.src = spot.link;
+    imgEl.alt = spot.name;
+    return el;
+}
+
+function renderImg(spot) {
+    const cards = document.querySelector('#cards')
+    const el = cloneElement(spot);
+    cards.prepend(el);
+}
+
+
+initialCards.forEach(renderImg);
